d285fa4b78347e94f881e56fbae87d143cda8530
aviaryan <avi.aryan123@gmail.com>
2015-11-15 14:08:54 +0530

    add token login

--- login.php
+++ login.php
@@ -5,42 +5,68 @@
 	include("functions/login_functions.php");
 	include("functions/commonfunctions.php");
 
-	// dis infect
-	$username = mysqli_real_escape_string( $con, $_POST["username"] );
-	$password = mysqli_real_escape_string( $con, $_POST["password"] );
+	$r = array();
+	foreach ($_POST as $key => $value) {
+		$r[$key] = mysqli_real_escape_string( $con, $value );
+	}
 
 	$rarr = array('status' => 0);
 
-	$queryuser = "select * from eyeds where username='{$username}' AND phash='{$password}'";
-	$resultuser = mysqli_query($con, $queryuser);
-	if ( mysqli_num_rows($resultuser) > 0 ){ // pass correct
-
-		// ALL USER DATA
-		$userarr = mysqli_fetch_assoc( $resultuser );
-		$userid = $userarr['id'];
-		$query = "select * from auths where id='{$userid}'";
-		$result = mysqli_query($con, $query);
-		$randstr = generateToken();
-		
-		if ( mysqli_num_rows($result) > 0 ){ // already token
-			$query = "update auths set token='{$randstr}' where id='{$userid}'";
-			$result = mysqli_query($con, $query);
-		} else { // create token
-			$query = "insert into auths values ('{$userid}', '{$randstr}')";
-			$result = mysqli_query($con, $query);
-		}
-
-		if ($result){
-			$rarr['token'] = $randstr;
-			$rarr = array_merge($rarr, $userarr);
-			die( json_encode($rarr) );
-		} else {
+	if (array_key_exists("token", $r)){
+		// authenticate with token
+		if ( tokenvalid($r['id'], $r['token']) ){
+			$token = generateToken();
+			$res = setToken($r['id'], $token);
+			if ($res){
+				$res = execQuery("select * from eyeds where id={$r['id']}");
+				$userarr = mysqli_fetch_assoc($res);
+				$rarr['token'] = $token;
+				die( json_encode(array_merge($rarr, $userarr)) );
+			} else
+				makeError(4);
+		} else
 			makeError(2);
-		}
+
+	} else if (array_key_exists("password", $r)){
+		// login with password
+		$queryuser = "select * from eyeds where username='{$r['username']}' AND phash='{$r['password']}'";
+		$resultuser = mysqli_query($con, $queryuser);
+		if ( mysqli_num_rows($resultuser) > 0 ){ // pass correct
+			// ALL USER DATA
+			$userarr = mysqli_fetch_assoc( $resultuser );
+			$randstr = generateToken();
+			$result = setToken($userarr['id'], $randstr);
+
+			if ($result){
+				$rarr['token'] = $randstr;
+				$rarr = array_merge($rarr, $userarr);
+				die( json_encode($rarr) );
+			} else
+				// db error
+				makeError(2);
+		} else
+			// wrong credentials
+			makeError(4);
 
 	} else {
-		makeError(4);
+		// wrong option
+		makeError(1);
 	}
 
+
+	function setToken($id, $token){
+		global $con;
+		$query = "select * from auths where id='{$id}'";
+		$result = mysqli_query($con, $query);
+		
+		if ( mysqli_num_rows($result) > 0 ){ // already token
+			$query = "update auths set token='{$token}' where id='{$id}'";
+			$result = mysqli_query($con, $query);
+		} else { // new token
+			$query = "insert into auths values ('{$id}', '{$token}')";
+			$result = mysqli_query($con, $query);
+		}
+		return $result;
+	}
 
 ?>
--- scripts/login.py
+++ scripts/login.py
@@ -3,7 +3,7 @@ import hashlib
 import json
 
 server = "http://collegare.eu5.org"
-# server = "http://localhost/collegare-server"
+server = "http://localhost/collegare-server"
 
 username="test2"
 password="qwerty"
@@ -15,6 +15,13 @@ payload = { 'username': username , 'password': phash }
 
 r = requests.post(server + "/login.php", data = payload)
 
+if r.ok:
+	json_res = json.loads( bytes.decode(r.content) )
+	print( json.dumps(json_res, indent=4, sort_keys=True) )
+
+# log 2
+payload = { 'id': json_res['id'], 'token': json_res['token'] }
+r = requests.post(server + "/login.php", data = payload)
 if r.ok:
 	json_res = json.loads( bytes.decode(r.content) )
 	print( json.dumps(json_res, indent=4, sort_keys=True) )
